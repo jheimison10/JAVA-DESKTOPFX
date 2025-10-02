73d02581d4749f5d7539e100c26e546cca1beb2d
jheimison10 <jheimison_gomes@hotmail.com>
2025-10-01 20:48:55 -0400

    Projeto swing + javaFX

--- desktop/src/main/java/senac/Main.java
+++ desktop/src/main/java/senac/Main.java
@@ -1,7 +1,21 @@
 package senac;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello JavaFX!");
+        Scene scene = new Scene(label, 400, 200);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX Teste");
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
     }
 }
